9c70da0304f0c5ab51b9c9841c78c95e3dc070f8
Kauan Mattos <kauanosmattos@gmail.com>
2026-08-13 20:49:46 -0300

    feat: Limpeza do boiler-plate. Criando components com fragment.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,18 +6,21 @@ export default function App() {
   //Declaração de variáveis e
   //Constantes etc;
 
+  const nome:string = "Kauan";
+  const sobrenome:string = "Mattos";
+
   return(
     <div>
       {/**Aqui colocamos a 
        * Apresentação
        */}
-       <Cabecalho/>
+       <Cabecalho />
        <main>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus iusto ex nulla odio    cumque distinctio eos quaerat, eius numquam ipsum quod hic reiciendis accusamus voluptas impedit molestias porro? Maxime rem, omnis maiores impedit illum enim iusto quae possimus. Nihil magnam vero ipsa quam natus sequi nostrum voluptas deserunt assumenda!
+          <b>{`Nome:${nome} Sobrenome:${sobrenome}`}</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus iusto ex nulla odio cumque distinctio eos quaerat, eius numquam ipsum quod hic reiciendis accusamus voluptas impedit molestias porro? Maxime rem, omnis maiores impedit illum enim iusto quae possimus. Nihil magnam vero ipsa quam natus sequi nostrum voluptas deserunt assumenda!
         </p>
        </main>
-        <Rodape/>
+        <Rodape />
     </div>
   );
 }
--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,11 @@
+import Botao from "../Botao/botao"
+
 export default function Cabecalho() {
 
 
     return(
         <header>
+            <Botao/>
             <h1>Projeto React</h1>
             <h2>Autor: Eu</h2>
         </header>
