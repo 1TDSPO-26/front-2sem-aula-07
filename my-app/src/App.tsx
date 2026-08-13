6cf83b7a67e4c6c1b4e1cf9d98243e9ee40067f6
matheusruiz-07 <matheus.rgiatti@gmail.com>
2026-08-12 23:11:09 -0300

    feat: Meu primeiro componente integrado.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/rodape";
 
 export default function App() {
   //declaracao de variaveis e 
@@ -12,7 +13,8 @@ export default function App() {
       <Cabecalho/>
     <main>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita minima assumenda perspiciatis repellendus illum delectus recusandae id fugiat, corrupti aut molestias rerum quia quasi nihil nulla, asperiores mollitia voluptas! Debitis.</p>
-       </main>
+    </main>
+       <Rodape/>
     </div>
   );
 
---
+++ my-app/src/components/Rodape/rodape.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Rodape(){
+
+    return (
+        <h2>Meu Componente</h2>
+
+
+    );
+
+}
