7594cd21e2d453aba5f8d66e4f9154c3e54ae173
melissafiuza <melfiuza153@gmail.com>
2026-08-12 23:11:17 -0300

    feat: Meu primeiro componente integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";  
+export default function App(){
+  return(
+    <div>
+      <Cabecalho/>
+      <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga tenetur, qui nihil atque fugit cumque quam ad. Veniam nihil eum quisquam laboriosam odit fugit ipsum, debitis odio cumque enim doloremque?</p>
+      </main>
+      <Rodape/>
+    </div>
+  );
+}
