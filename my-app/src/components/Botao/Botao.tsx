dbd6a714b79b4245b35e2774f7bb3cd227f53f00
di-zanon <zanon_harnisch@hotmail.com>
2026-08-13 20:45:58 -0300

    feat : Limpeza do boilerplate. Criando componentes com fragment.

---
+++ my-app/src/components/Botao/Botao.tsx
@@ -0,0 +1,9 @@
+
+export default function Botao() {
+
+    return (
+        <>
+            <button onClick={() => alert('TESTE')}>CLICK</button>
+        </>
+    );
+}
--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,10 @@
+import Botao from "../Botao/Botao";
 
 export default function Cabecalho() {
 
     return (
         <header>
+            <Botao />
             <h1>Projeto React</h1>
             <h2>Autor: Eu</h2>
         </header>
