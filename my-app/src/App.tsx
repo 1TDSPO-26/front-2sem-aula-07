ba0324333c11eb8eb088469db741cd3f1f073b01
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-08-12 23:11:13 -0300

    feat : Meu primeiro componente integrado.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho"
+import Rodape from "./components/Rodape/Rodape";
+
+
+export default function App() {
+  //Declaração de variáveis e
+  //constantes etc;
+
+  return (
+    <div>
+      {/**Aqui colocamos a
+       * Apresentação
+       */}
+      <Cabecalho />
+      <main>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias molestiae blanditiis omnis quos architecto maxime iure. Consectetur beatae dolores quaerat quod accusantium quidem similique adipisci reprehenderit dolorem fugit dolorum error, ea eius autem perspiciatis nulla vitae quae! Qui aut obcaecati incidunt possimus porro quibusdam delectus at? Soluta cupiditate in voluptates?
+        </p>
+      </main>
+      <Rodape />
+    </div>
+  );
+
+}
