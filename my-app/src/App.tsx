54a4b8614b66330fae2987cead2b6b6bfd4b46d5
borbagustavo <gustavo.borba.27@gmail.com>
2026-08-12 23:11:12 -0300

    feat: projeto em react, feito os primeiros components

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cabecalho from './components/Cabecalho/Cabecalho'
+
+function App() {
+  //declaaração de variáveis e funções do componente
+  //constantes 
+  return (
+    <div>
+     <Cabecalho/>
+     <main>
+       <p>Conteúdo principal</p>
+     </main>
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cabecalho() {
+  return (
+    <header>
+        <h1>Projeto React</h1>
+        <h2>Autor: João</h2>
+    </header>
+  )
+}
+
+export default Cabecalho
