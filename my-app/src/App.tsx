ccec4b3944733d7b957e9092aa7859239d79cca0
Enzo-N-Freitas <enzo_negrao_freitas@outlook.com>
2026-08-12 23:11:10 -0300

    feat: Meu primeiro componente integrado.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";
+
+export default function App() {
+  return (
+    <div>
+      <Cabecalho/>
+      <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veniam ea iure delectus vitae ullam quisquam, aliquid, porro dolor eos atque voluptatibus consectetur quo labore ducimus, omnis sunt sapiente animi nostrum? Quisquam ex tenetur enim debitis dolor sit qui magnam dignissimos doloribus error deserunt neque accusamus dolore nemo, atque nulla repudiandae nam harum molestiae accusantium culpa architecto quam. Illum, eum laboriosam perferendis, nesciunt quia sed quasi repellendus porro dolorem assumenda adipisci at aliquam ipsum illo exercitationem eius unde repudiandae nostrum? Nobis, minus porro. Mollitia ut tenetur nisi, perferendis iste aliquam quo maiores, cupiditate quaerat hic molestias minus animi ratione. Dignissimos!</p>
+      </main>
+      <Rodape />
+    </div>
+  );
+}
