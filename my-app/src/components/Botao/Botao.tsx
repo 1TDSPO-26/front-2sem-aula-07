d549927230a9be8d083585bd99d965618b0eb735
Matheus <rm573416@fiap.com.br>
2026-08-13 20:44:58 -0300

    feat: Limpeza de Boiler-plate e criando componentes

---
+++ my-app/src/components/Botao/Botao.tsx
@@ -0,0 +1,10 @@
+export default function Botao() {
+
+    return(
+        <>
+            <button onClick={()=>alert('TESTE')}>CLICK</button>
+
+        </>
+    )
+
+}
--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,6 +1,9 @@
+import Botao from "../Botao/Botao"
+
 export default function Cabecalho() {
     return(
         <header>
+            <Botao />
             <h1>Projejo React</h1>
             <h2>Autor: eu</h2>
         </header>
