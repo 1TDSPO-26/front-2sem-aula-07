74b10d528b60e7d3d9739dc3e47e100bd277cf0f
abrantes1 <almeidalucasbru@gmail.com>
2026-08-12 23:11:16 -0300

    feat: Meu primeiro componente integrado.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import Cabecalho from "./assets/components/Cabecalho/Cabecalho";
+import Rodape from "./assets/components/Rodape/Rodape";
+
+
+
+export default function App() {
+  //Declaçao de variaveis e 
+  // constantes etc;
+
+
+  return (
+    <div>
+      {
+        /*Aqui colocamos a apresentaçao */
+      }
+      <Cabecalho />
+      <main><p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore optio cupiditate eius, nobis, atque laborum eos doloribus consectetur, hic cum cumque ipsa enim pariatur! Debitis, recusandae officia. Aliquam, quidem aperiam.
+      </p></main>
+      <Rodape />
+    </div>
+  );
+
+}
---
+++ my-app/src/assets/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,9 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>projeto React</h1>
+            <h2> Autor:Eu</h2>
+        </header>
+    );
+
+}
