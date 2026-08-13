7aead22fbf67e6d70b3ccbd3d1b30efd21a0aa31
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-08-13 20:59:04 -0300

    feat : Passando dados de props.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho"
 import Rodape from "./components/Rodape/Rodape";
+import Saudacao from "./components/Saudacao/Saudacao";
 
 
 export default function App() {
@@ -16,6 +17,11 @@ export default function App() {
        */}
       <Cabecalho />
       <main>
+
+        <div>
+          <Saudacao nome={"José"}/>
+        </div>
+
         <p>
           <b>{`Nome:${nome} Sobrenome:${sobrenome}`}</b> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias molestiae blanditiis omnis quos architecto maxime iure. Consectetur beatae dolores quaerat quod accusantium quidem similique adipisci reprehenderit dolorem fugit dolorum error, ea eius autem perspiciatis nulla vitae quae! Qui aut obcaecati incidunt possimus porro quibusdam delectus at? Soluta cupiditate in voluptates?
         </p>
