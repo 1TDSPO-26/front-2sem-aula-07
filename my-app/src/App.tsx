c81fc0e4290a9abcadffe2623df974e0ece62b61
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-08-13 20:45:07 -0300

    feat : Limpezxa do boiler-plate. Criando componentes com fragment.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,9 @@ export default function App() {
   //Declaração de variáveis e
   //constantes etc;
 
+  const nome:string = "Alê";
+  const sobrenome:string = "Carlos";
+
   return (
     <div>
       {/**Aqui colocamos a
@@ -14,7 +17,7 @@ export default function App() {
       <Cabecalho />
       <main>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias molestiae blanditiis omnis quos architecto maxime iure. Consectetur beatae dolores quaerat quod accusantium quidem similique adipisci reprehenderit dolorem fugit dolorum error, ea eius autem perspiciatis nulla vitae quae! Qui aut obcaecati incidunt possimus porro quibusdam delectus at? Soluta cupiditate in voluptates?
+          <b>{`Nome:${nome} Sobrenome:${sobrenome}`}</b> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias molestiae blanditiis omnis quos architecto maxime iure. Consectetur beatae dolores quaerat quod accusantium quidem similique adipisci reprehenderit dolorem fugit dolorum error, ea eius autem perspiciatis nulla vitae quae! Qui aut obcaecati incidunt possimus porro quibusdam delectus at? Soluta cupiditate in voluptates?
         </p>
       </main>
       <Rodape />
