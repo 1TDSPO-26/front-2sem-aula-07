320989d222c2a185cf3938becb815c0b4e8ab592
matheusruiz-07 <matheus.rgiatti@gmail.com>
2026-08-12 23:01:08 -0300

    feat: Primeiro codigo em React

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
+export default function App() {
+  //declaracao de variaveis e 
+  //constantes etc;
+
+  return(
+    <div>
+      {/**aqui colocamos a
+       * apresentação
+       */}
+      <Cabecalho/>
+    <main>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita minima assumenda perspiciatis repellendus illum delectus recusandae id fugiat, corrupti aut molestias rerum quia quasi nihil nulla, asperiores mollitia voluptas! Debitis.</p>
+       </main>
+    </div>
+  );
+
+}
---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Cabecalho(){
+
+    return(
+        <header>
+            <h1>Projeto React</h1>
+            <h2>Autor:Eu</h2>
+        </header>
+
+    );
+
+}
