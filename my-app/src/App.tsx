ea5062ecb6ad29a21d3a2da3dec7434722a5c3d7
melissafiuza <melfiuza153@gmail.com>
2026-08-13 20:52:02 -0300

    feat: Limpeza do boiler-plate. Criando componentes com fragment

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,7 @@ export default function App(){
     <div>
       <Cabecalho/>
       <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga tenetur, qui nihil atque fugit cumque quam ad. Veniam nihil eum quisquam laboriosam odit fugit ipsum, debitis odio cumque enim doloremque?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ut! Aspernatur architecto perspiciatis voluptatem optio ea et praesentium quaerat accusamus quia odio velit, tenetur iure, dolorem odit maxime ab consequatur impedit fugiat dolores minima aliquam provident nisi. Veniam itaque ad voluptatum, officiis cupiditate sint nihil. Tempora pariatur minus iure error.</p>
       </main>
       <Rodape/>
     </div>
---
+++ my-app/src/components/Botao/Botao.tsx
@@ -0,0 +1,8 @@
+export default function Botao(){
+  return(
+   <>
+    <button onClick={() => alert('Botão 1 clicado')}>Botão</button>
+    <button>Botão 2</button>
+    </> 
+  );
+}
