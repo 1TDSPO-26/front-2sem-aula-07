cc6ca09a04d8f7f70672ca69ae149e39eb1f9072
allex1930 <allexoliveiraa756@gmail.com>
2026-08-12 23:11:07 -0300

    feat: Meu primeiro componente integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,20 @@
+import Cabecalho from "./components/Cabecalho/Cabelho"
+import Rodape from "./components/Rodape/Rodape";
+
+  export default function App(){
+    return (
+      <div>
+        <Cabecalho/>
+        <main>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci aspernatur, consequatur numquam officia placeat nam. Qui consequatur ad vel corrupti quidem iusto animi error distinctio unde, consequuntur incidunt doloribus assumenda harum alias non voluptatem saepe officia commodi eaque facere, porro itaque eum? Nihil asperiores minima voluptatem inventore consequuntur at suscipit!
+          </p>
+        </main>
+        <Rodape/>
+
+      </div>
+    );
+
+
+  }
+  
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+        <footer>
+            <h1>Rodapé</h1>
+        </footer>
+    )
+}
