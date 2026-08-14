c87ed604f8922b52d7ff0d293a8f0063ee637559
melissafiuza <melfiuza153@gmail.com>
2026-08-13 21:00:57 -0300

    feat: Iniciando com props.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,15 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";  
+import Saudacao from "./components/Saudacao/Saudacao";
 export default function App(){
   return(
     <div>
       <Cabecalho/>
       <main>
+
+        <div>
+          <Saudacao nome={"Mel"}  />
+        </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ut! Aspernatur architecto perspiciatis voluptatem optio ea et praesentium quaerat accusamus quia odio velit, tenetur iure, dolorem odit maxime ab consequatur impedit fugiat dolores minima aliquam provident nisi. Veniam itaque ad voluptatum, officiis cupiditate sint nihil. Tempora pariatur minus iure error.</p>
       </main>
       <Rodape/>
---
+++ my-app/src/components/Saudacao/Saudacao.tsx
@@ -0,0 +1,3 @@
+export default function Saudacao({ nome }: { nome: string }) {
+  return <h1>Olá, {nome}!</h1>;
+}
