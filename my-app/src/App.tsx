7c6edf7516c10645ce616a3a162210376f8c6abb
felipeads12 <lipepassos180208gmail.com>
2026-08-13 21:00:41 -0300

    Feat: Passando dados de props.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import Cabecalho from "./components/Cabacalho/Cabecalho"
 import Rodape from "./components/Rodape/Rodape"
+import { Saudacao } from "./components/Saudacao/Saudacao";
 
 export default function App() {
   
@@ -14,6 +15,11 @@ export default function App() {
       <h1>Meu componente</h1>
       <Cabecalho />
       <main>
+
+        <div>
+        <Saudacao nome={"Felipe"}/>
+        </div>
+
         <p>
           <b>{nome} {sobrenome}</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt laborum fuga nostrum obcaecati minus voluptatibus, assumenda perferendis veniam rem molestias eaque! Voluptate libero quo corrupti eius mollitia iste sed cupiditate debitis itaque! Veritatis, soluta! Minima voluptatum pariatur eligendi facere, obcaecati hic quidem voluptatem! Deleniti ullam non necessitatibus officiis. Optio, sed.
         </p>
