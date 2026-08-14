115c8205bacf1fb9c465648e64ba795c85d4e1b7
cesarledres <cesarledrescv@gmail.com>
2026-08-13 21:04:58 -0300

    feat : Passando dados de props.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho"
 import Rodape from "./components/Rodape/Rodape"
+import Saudacao from "./components/Saudacao/Saudacao";
 
 
 export default function App(){
@@ -14,6 +15,11 @@ export default function App(){
       <Cabecalho/>
       
       <main>
+
+        <div>
+          <Saudacao nome={"jose"}/>
+        </div>
+
         <p>
           <b>{`Nome:${nome} sobrenome:${sobrenome}`}</b> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, labore dicta voluptatum aperiam eveniet consequatur. Labore aut fugit laborum explicabo incidunt vitae. Distinctio et dicta expedita temporibus suscipit! Adipisci nam voluptas quisquam unde, nemo tenetur possimus sapiente est eveniet saepe, qui enim voluptate, voluptatum impedit. Quis, hic! At, quae reprehenderit.
         </p>
