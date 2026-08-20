e7840b3fedf26f796714b15a3baa80763ea820cd
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-08-19 23:00:48 -0300

    feat: Tipagem de props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,17 @@
 
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
-import Saudacao from "./components/Saudacao/Saudacao";
 
 export default function App() {
-
-  const nome:string = "Murilo";
-  const sobrenome:string = "Cruz";
 
   return (
     <div>
       {/**Aqui colocamos a 
          * Apresentação
          */}
-      <Cabecalho/>
-      <main>
-        
-        <div>
-          <Saudacao nome={"Murilo"}/>
-        </div>
-
-        <p><b>{`Nome: ${nome} ${sobrenome}`}</b>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt neque aut molestias. Nulla commodi adipisci molestias neque? Quasi quos esse, nobis provident, expedita accusantium possimus, mollitia libero placeat aspernatur non eaque iusto deserunt suscipit eos molestias laboriosam vitae delectus praesentium exercitationem alias consectetur? Obcaecati fugiat animi maiores saepe doloremque veniam.</p></main>
+      <Cabecalho />
+      <Conteudo />
       <Rodape />
     </div>
   );
