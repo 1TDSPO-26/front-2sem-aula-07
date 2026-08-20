59accac54ecd76082ff2871c8f77bdf17f8beed4
Kauan Mattos <kauan.sfrizo@gmail.com>
2026-08-19 23:01:23 -0300

    feat: Tipagem de props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,10 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho"
+import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
-import Saudacao from "./components/Saudacao/Saudacao";
-
 
 export default function App() {
   //Declaração de variáveis e
   //Constantes etc;
-
-  const nome:string = "Kauan";
-  const sobrenome:string = "Mattos";
 
   return(
     <div>
@@ -16,16 +12,7 @@ export default function App() {
        * Apresentação
        */}
        <Cabecalho />
-       <main>
-
-        <div>
-          <Saudacao nome={"José"}/>
-        </div>
-
-        <p>
-          <b>{`Nome:${nome} Sobrenome:${sobrenome}`}</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus iusto ex nulla odio cumque distinctio eos quaerat, eius numquam ipsum quod hic reiciendis accusamus voluptas impedit molestias porro? Maxime rem, omnis maiores impedit illum enim iusto quae possimus. Nihil magnam vero ipsa quam natus sequi nostrum voluptas deserunt assumenda!
-        </p>
-       </main>
+        <Conteudo />
         <Rodape />
     </div>
   );
---
+++ my-app/src/components/Card/Card.tsx
@@ -0,0 +1,12 @@
+
+type TipoProps = {
+  title:string;
+}
+
+export default function Card(props: TipoProps) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+    </div>
+  )
+}
