f44f1add2b99fa7c5238c2ec8cd917fcb83a72b0
di-zanon <zanon_harnisch@hotmail.com>
2026-08-19 23:01:21 -0300

    feat : Tipagem de props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
-import Saudacao from "./components/Saudacao/Saudacao";
+
 
 export default function App() {
 
@@ -13,15 +14,7 @@ export default function App() {
        * Apresentação
        */}
       <Cabecalho />
-      <main>
-
-        <div>
-          <Saudacao nome={"José"} />
-        </div>
-
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid sed totam non suscipit, recusandae harum facilis dolore alias earum veniam soluta cupiditate, numquam quibusdam hic accusantium ducimus quos tenetur incidunt.</p>
-      </main>
-
+      <Conteudo />
       <Rodape />
     </div>
   );
