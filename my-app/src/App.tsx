62533c923d3acfe8fc973ce3491d5106657acc05
MatheuSegura <matheus.segura.anacleto@gmail.com>
2026-08-19 23:02:36 -0300

    feat: Tipagem de props

---
+++ my-app/src/App.tsx
@@ -0,0 +1,21 @@
+import Cabecalho from "./components/cabecalho/Cabecalho"
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+
+
+export default function App() {
+  //Declaração de variáveis e
+  //constantes etc;
+
+
+  return(
+    <div>
+      {/**Aqui colocamos a 
+       * Apresentação
+       */}
+       <Cabecalho/>
+       <Conteudo/>
+       <Rodape/>
+    </div>
+  );
+}
