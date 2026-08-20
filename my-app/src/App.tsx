fbc9707c243014fe32e1ce5639f1a3c1b470e49b
theo4321 <theo.caria.r6@gmail.com>
2026-08-19 23:04:01 -0300

    feat: Tipagem de props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,30 +1,18 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho"
+import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape"
-import Saudacao from "./components/Saudacao/Saudacao";
+
 
 
 export default function App(){
 //Declareção de variaveis e constantes etc;
 
-  const nome:string ="ale";
-  const sobrenome:string = "carlos";
 
   return(
     <div>
       {/**Aqui colocamos a apresentação */}
       <Cabecalho/>
-      
-      <main>
-
-        <div>
-          <Saudacao nome={"jose"}/>
-        </div>
-
-        <p>
-          <b>{`Nome:${nome} sobrenome:${sobrenome}`}</b> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, labore dicta voluptatum aperiam eveniet consequatur. Labore aut fugit laborum explicabo incidunt vitae. Distinctio et dicta expedita temporibus suscipit! Adipisci nam voluptas quisquam unde, nemo tenetur possimus sapiente est eveniet saepe, qui enim voluptate, voluptatum impedit. Quis, hic! At, quae reprehenderit.
-        </p>
-      </main>
-
+      <Conteudo/>
       <Rodape/>
     </div>
   );
