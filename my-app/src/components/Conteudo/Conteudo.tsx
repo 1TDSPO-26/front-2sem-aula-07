21d96a32281c14c8654fb3fce97a88ddb92a554c
cesarledres <cesarledres@gmail.com>
2026-08-19 23:02:18 -0300

    feat : Tipagem de props

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,11 @@
+import Card from "../Card/Card";
+
+export default function Conteudo() {
+  return (
+    <main>
+        <div>
+            <Card title="Java" />
+        </div>
+    </main>
+  )
+}
