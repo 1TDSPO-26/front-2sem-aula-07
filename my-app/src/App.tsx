cd5ad4cc95d3b63ec38142e289af613552552039
matheusleite21 <matheus.leitectj@gmail.com>
2026-08-19 23:00:58 -0300

    feat: Tipagem de Props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
-import Saudacao from "./components/Saudacao/Saudadao";
+
 
 export default function App(){
   //Declaração de variáveis e 
@@ -9,15 +10,8 @@ export default function App(){
   return(
     <div>
       {/**Aqui colocamos a Apresentação */}
-    <Cabecalho/>
-    <main>
-      <div>
-        <Saudacao nome={"José"}  />
-      </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam aspernatur dolorem aliquam quasi ipsum vitae optio ea inventore. Nemo, dolorem fuga. Maiores neque quaerat numquam recusandae optio, quia tenetur officiis.
-        </p>
-      </main>
+      <Cabecalho/>
+      <Conteudo/>
       <Rodape/>
     </div>
   );
