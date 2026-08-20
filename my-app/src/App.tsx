3085d3dd13015871c3bb94f9f910e01c873eea0b
Enzo-N-Freitas <enzo_negrao_freitas@outlook.com>
2026-08-19 22:44:09 -0300

    atualizando projeto

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,30 @@
-import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Cabecalho from "./components/Cabecalho/Cabecalho"
 import Rodape from "./components/Rodape/Rodape";
+import Saudacao from "./components/Saudacao/Saudacao";
+
 
 export default function App() {
+  //Declaração de variáveis e
+  //constantes etc;
+
+  const nome: string = "Alê";
+  const sobrenome: string = "Carlos";
+
   return (
     <div>
-      <Cabecalho/>
+      {/**Aqui colocamos a
+       * Apresentação
+       */}
+      <Cabecalho />
       <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veniam ea iure delectus vitae ullam quisquam, aliquid, porro dolor eos atque voluptatibus consectetur quo labore ducimus, omnis sunt sapiente animi nostrum? Quisquam ex tenetur enim debitis dolor sit qui magnam dignissimos doloribus error deserunt neque accusamus dolore nemo, atque nulla repudiandae nam harum molestiae accusantium culpa architecto quam. Illum, eum laboriosam perferendis, nesciunt quia sed quasi repellendus porro dolorem assumenda adipisci at aliquam ipsum illo exercitationem eius unde repudiandae nostrum? Nobis, minus porro. Mollitia ut tenetur nisi, perferendis iste aliquam quo maiores, cupiditate quaerat hic molestias minus animi ratione. Dignissimos!</p>
+
+        <div>
+          <Saudacao nome={"José"} />
+        </div>
+        
       </main>
       <Rodape />
     </div>
   );
+
 }
