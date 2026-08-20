b3465b0576642922836e08726aba8d3fbb7444c3
felipeads12 <lipepassos180208gmail.com>
2026-08-19 23:01:20 -0300

    feat: Tipagem de props.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Cabecalho from "./components/Cabacalho/Cabecalho"
+import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape"
 import { Saudacao } from "./components/Saudacao/Saudacao";
 
@@ -14,16 +15,7 @@ export default function App() {
       {/**Aqui colocamos a apresentação */}
       <h1>Meu componente</h1>
       <Cabecalho />
-      <main>
-
-        <div>
-        <Saudacao nome={"Felipe"}/>
-        </div>
-
-        <p>
-          <b>{nome} {sobrenome}</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt laborum fuga nostrum obcaecati minus voluptatibus, assumenda perferendis veniam rem molestias eaque! Voluptate libero quo corrupti eius mollitia iste sed cupiditate debitis itaque! Veritatis, soluta! Minima voluptatum pariatur eligendi facere, obcaecati hic quidem voluptatem! Deleniti ullam non necessitatibus officiis. Optio, sed.
-        </p>
-      </main>
+      <Conteudo/>
       <Rodape />
     </div>
 
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,12 @@
+import Card from '../Card/Card'
+import { Saudacao } from '../Saudacao/Saudacao'
+
+export default function Conteudo() {
+  return (
+    <div>
+         <main>
+                <Card title="Java"/>
+        </main>
+    </div>
+  )
+}
