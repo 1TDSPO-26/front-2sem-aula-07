3931c0146ac0519a3d4276c64d407a014464397c
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-08-26 22:21:52 -0300

    feat: instalando pacotes do react router e tailwindcss e aplicando exemplos

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -3,9 +3,9 @@ import Botao from "../Botao/Botao"
 export default function Cabecalho() {
 
     return (
-        <header>
+        <header className="bg-amber-200 w-screen text-black flex justify-around">
             <Botao/>
-            <h1>Projeto React</h1>
+            <h1 className="minhaClasse">Projeto React</h1>
             <h2>Autor: Eu</h2>
         </header>
         
