46ac8d638a1c076cc81cd182b35aa3229affe4c1
AnaMendes-25 <anaclaramendes2501@gmail.com>
2026-08-26 22:38:31 -0300

    feat: componentes de rotas criados.

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,8 @@
+
+export default function EditarProdutos() {
+  return(
+    <main>
+      <h2>Home</h2>
+    </main>
+  )
+}
---
+++ my-app/src/routes/Produtos/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Produtos() {
+  return(
+    <main>
+      <h2>Home</h2>
+    </main>
+  )
+}
