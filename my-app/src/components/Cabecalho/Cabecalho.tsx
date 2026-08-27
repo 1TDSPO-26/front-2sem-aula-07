6cd15e5e244d76825cdc5a2d6d2bb969b17c5bd9
AnaMendes-25 <anaclaramendes2501@gmail.com>
2026-08-26 22:22:49 -0300

    feat: pacotes do react instalados + tailwind css c exemplos iniciais

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -3,9 +3,9 @@ import Botao from "../Botao/Botao";
 export default function Cabecalho() {
 
   return (
-    <header>
+    <header className="bg-amber-300 w-screen text-blue-900 flex justify-around">
       <h1>Projeto React</h1>
-      <h2>Autor: Ana Mendes Sempre Linda</h2>
+      <h2 className="minhaClasse">Autor: Ana Mendes Sempre Linda</h2>
       <Botao/>
     </header>
   )
