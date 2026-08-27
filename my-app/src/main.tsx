8bdbfbc9a3e20e72284d5393a10848ec6a099a45
alexandreteixeiras <teixeiradossantosalexandre@gmail.com>
2026-08-26 22:59:40 -0300

    feat: Estrutura de rotas finalizadas.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./globals.css"
 
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+import Home from "./routes/home/index.tsx"
+import Produtos from "./routes/Produtos/index.tsx"
+import EditarProdutos from "./routes/EditarProdutos/index.tsx"
+import Error from "./routes/Error/index.tsx"
+import path from 'path'
+
+const router = createBrowserRouter([
+  {path:"/", element: <App/>, errorElement:Error, children:[
+    {path:"/", element: <Home/>},
+    {path:"/", element: <Produtos/>},
+    {path:"/", element: <EditarProdutos/>}
+  ]}
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
