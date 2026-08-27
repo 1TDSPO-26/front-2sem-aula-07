edc248180a4700ecc2a9747503816f6e86d496c4
ropark-tech <ropark.tech@proton.me>
2026-08-26 22:38:17 -0300

    feat: Componentes de rotas criados.

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,8 @@
+export default function EditarProdutos(){
+    return(
+        <main>
+            <h2>EditarProdutos</h2>
+        </main>
+    );
+
+}
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,8 @@
+export default function Error(){
+    return(
+        <main>
+            <h2>404 - Página não encontrda.</h2>
+        </main>
+    );
+
+}
