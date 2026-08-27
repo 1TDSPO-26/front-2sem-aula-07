7457f78a36d056f7e914fc1828d4ed7b0fb888e1
rm573416 <matheus.leitectj@gmail.com>
2026-08-26 22:23:46 -0300

    Feat: configurando e aplicando Tailwindcss e vite.

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -2,9 +2,9 @@ import Botao from "../Botao/Botao"
 
 export default function Cabecalho() {
     return(
-        <header>
-            <Botao />
-            <h1>Projejo React</h1>
+        <header className="bg-amber-200 w-screen text-black flex justify-around">
+            <Botao/>
+            <h1 className=".minhaClasse">Projejo React</h1>
             <h2>Autor: eu</h2>
         </header>
     );
