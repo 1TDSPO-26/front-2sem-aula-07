5f1f37ae25dc5ef0fa476c05fd0f6269994a13d7
allex1930 <allexoliveiraa756@gmail.com>
2026-08-26 23:00:04 -0300

    ajustes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,16 @@
+import { Outlet } from "react-router";
 import Cabecalho from "./components/Cabecalho/Cabecalho"
 import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
 
-
 export default function App() {
-  //Declaração de variáveis e
-  //constante e etc;
 
-  const nome: string = "Mateus";
-  const sobrenome: string = "Isaque";
   return (
     <div>
       <Cabecalho />
-      <Conteudo />
+      <Outlet />
       <Rodape />
     </div>
   );
+
 }
