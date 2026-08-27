23064d68aa0e0eab0724044b01a32c353be1512a
Kauan Mattos <kauan.sfrizo@gmail.com>
2026-08-26 22:40:05 -0300

    feat: Componentes de rotas criados.

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            <h2>Editar Produtos</h2>
+        </main>
+    );
+}
---
+++ my-app/src/routes/Produtos/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            <h2>Produtos</h2>
+        </main>
+    );
+}
