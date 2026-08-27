8170112815c2851173de8815ac7f3537c21e1a09
Mateus-Isaque <mateusisaque2007@gmail.com>
2026-08-26 23:01:37 -0300

    feat : Rotas criadas

--- my-app/src/components/Rodape/Rodape.tsx
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -2,7 +2,7 @@ export default function Rodape() {
 
     return(
         <footer>
-            <h2>© 2026 Meu Projeto React. Todos os direitos reservados para Mateus Isaque.</h2>
+            <h2 className="text-white">© 2026 Meu Projeto React. Todos os direitos reservados para Mateus Isaque.</h2>
         </footer>
     );
 }
