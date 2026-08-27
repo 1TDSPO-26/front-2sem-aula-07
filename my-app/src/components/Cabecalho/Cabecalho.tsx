36ea42c24d0814d6a99e4f300ce77ec09cd33b8d
Gracetti <igor.gra7@gmail.com>
2026-08-26 22:29:29 -0300

    Ajustes aula7

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,13 @@
-export default function Cabecalho(){
-  return(
-    <header>
-      <h1>Meu Projeto </h1>
-    </header>
-  );
+import Botao from "../Botao/Botao"
+
+export default function Cabecalho() {
+
+    return(
+        <header className="bg-amber-200 w-screen text-black flex justify-around">
+            <Botao/>
+            <h1 className="MinhaClasse">Projeto React</h1>
+            <h2 className="MinhaClasse">Autor:Eu</h2>
+        </header>
+    );
+    
 }
